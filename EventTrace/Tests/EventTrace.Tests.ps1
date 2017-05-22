200de2521ab5dd753df8bf57a8863354e3de6d3b
Matt Hastings <matthew.hastings@tanium.com>
2017-05-21 20:52:05 -0400

    Starting to add pester tests

--- EventTrace/Tests/EventTrace.Tests.ps1
+++ EventTrace/Tests/EventTrace.Tests.ps1
@@ -1,2 +1,8 @@
 Set-StrictMode -Version Latest
+$RootModuleDir = Split-Path $MyInvocation.MyCommand.Path -Parent
+$a = Resolve-Path "$RootModuleDir\.."
+$a
+$Module = "$a\EventTrace.psd1"
+Import-Module $Module -Force -ErrorAction Stop
+$ErrorActionPreference = 'Stop'
 
